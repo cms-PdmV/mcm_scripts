4f59c0d70803101e0e7a794ffa8a0fbd1c53de8f
pgunnell <paolo.gunnellini@cern.ch>
2019-05-15 16:12:35 +0200

    Update runningCrossSection.sh

--- CrossSectionCalculation/runningCrossSection.sh
+++ CrossSectionCalculation/runningCrossSection.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#Usage runningCrossSection.sh 'Name_of_MiniAOD_sample_one_wants_to_calculate_the_cross_section_for'
 source /afs/cern.ch/user/p/pgunnell/public/getCookie.sh
 python CrossSectionEvaluation.py $1
 source script.sh
